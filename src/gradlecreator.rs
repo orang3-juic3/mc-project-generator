7030e8bd11f58371c9aae26f9285b1d8903b2ef8
Alex <alexzatstone@gmail.com>
2022-09-05 04:43:50 +0100

    use command to test for gradle rather than regex magic

--- src/gradlecreator.rs
+++ src/gradlecreator.rs
@@ -4,6 +4,7 @@ use std::path::{Path, PathBuf};
 use std::env;
 use regex::Regex;
 use std::process::{Command, Stdio};
+use std::io::Read;
 
 pub struct Gradle {
     cli: Rc<Cli>,
@@ -36,7 +37,14 @@ impl Gradle {
                  (Example of correct (Windows) directory: F:\\gradle\\dists\\gradle-6.8-bin\\gradle-6.8\\bin\\)")
             }
         }
-        if let Some(paths) = env::var_os("PATH") {
+        let wait = if cfg!(target_os = "windows") {
+            Command::new("cmd").args(["/C", "gradle --no-daemon -v"]).stdout(Stdio::piped()).spawn()
+        } else {
+            Command::new("sh").args(["-c", "gradle --no-daemon -v"]).stdout(Stdio::piped()).spawn()
+        };
+        dbg!(&wait);
+        if let Ok(mut child) = wait {
+        /*if let Some(paths) = env::var_os("PATH") {
             let gradle : Vec<PathBuf> = env::split_paths(&paths).filter(|it| {
                     let mut sep = String::from(std::path::MAIN_SEPARATOR);
                     if sep == r"\" {
@@ -51,7 +59,9 @@ impl Gradle {
                     cli: Rc::clone(&cli),
                     path: gradle[0].clone()
                 }
-            }
+            }*/ // Why do regex garbage when you can just do the command?
+            dbg!("tyt");
+            println!("{}", String::from_utf8(child.wait_with_output().unwrap().stdout).unwrap());
         }
 
         return Self {
